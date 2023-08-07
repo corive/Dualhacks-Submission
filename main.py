539555207952838a0894c5e426eb20ef4760a3e0
Patrick Hayes <138741129+corive@users.noreply.github.com>
2023-08-07 09:36:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ app = Flask(__name__)
 def hello():
     return "we def started this hackathon on time"
 
-@app.route("/hyein")
-def i_think_you_can_name_this_function_anything():
-    return "mul juseyo!"
-
 app.run(debug=True)
