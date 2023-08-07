3cef59f4f1be51c4583ae7eb84e7b30cd9a6fc0c
Marko Kupresanin <kupresaninmarko2@gmail.com>
2023-08-06 17:26:23 -0700

    demonstration!

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 def hello():
     return "we def started this hackathon on time"
 
+@app.route("/hyein")
+def i_think_you_can_name_this_function_anything():
+    return "mul juseyo!"
+
 app.run(debug=True)
